7ec0f1ea16e36d86ec43db7687d3e7bc885a4621
adams <adamskiptalam0@gmail.com>
2025-03-27 15:03:40 +0300

    Add call to action in the home page

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ type, name }) => {
+	return (
+		<button type={type}>
+			{name}
+		</button>
+	);
+};
+
+export default Button;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
+import Button from "../components/Button";
 export default function Home() {    
     return (
 			<div className={styles.homeContainer}>
 				<Header />
+				<div className={styles.homeAction}>
+					<h1>SHOP SMARTER . LIVE BETTER</h1>
+					<p>
+						Find everything you need at unbeatable prices. Quality products,
+						fast shipping and amazing deals - all in one place.
+					</p>
+					<Button type='button' name='SHOP NOW' />
+				</div>
 			</div>
 		);
 }
