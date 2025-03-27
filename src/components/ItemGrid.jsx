cdb6027f79d17e9be62453bd4a8e73e0db0f2fa3
adams <adamskiptalam0@gmail.com>
2025-03-28 02:02:25 +0300

    Add products using API

--- src/components/ItemGrid.jsx
+++ src/components/ItemGrid.jsx
@@ -1,24 +1,51 @@
 import { useState, useEffect } from "react";
 import ItemCard from "./ItemCard";
 import styles from "../styles/ItemGrid.module.css"; // Assuming you have a CSS module for styling
-import shoeImage from "../assets/shoe.jpg";
+// import shoeImage from "../assets/shoe.jpg";
 
 const ItemGrid = () => {
-	const [items, setItems] = useState([
-		{ id: 1, name: "adidas", url: shoeImage },
-		{ id: 2, name: "nike", url: shoeImage },
-		{ id: 3, name: "puma", url: shoeImage },
-		{ id: 4, name: "canvas", url: shoeImage },
-		{ id: 5, name: "rats", url: shoeImage },
-		{ id: 6, name: "niki", url: shoeImage },
+	const [items, setItems] = useState(
+        localStorage.getItem("products") ? JSON.parse(localStorage.getItem("products")) : []
+    );
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
 
-		// Add more items as needed
-	]);
+	useEffect(() => {
+		const fetchProducts = async () => {
+			try {
+				const response = await fetch("https://fakestoreapi.com/products/");
+				if (!response.ok) {
+					throw new Error("Network response was not ok" + response.status);
+				}
+				const data = await response.json();
+				console.log(data);
+				setItems(data);
+			} catch (error) {
+				console.error("Error fetching products:", error);
+			} finally {
+				setLoading(false);
+				setError(error);
+			}
+		};
+		fetchProducts();
+	}, [error]);
+
+    useEffect(() => {
+        localStorage.setItem("products", JSON.stringify(items));
+    }, [items]);
+
+	if (loading) {
+		return <div>Loading...</div>; // Display a loading message
+	}
+
+	if (error) {
+		return <div>Error: {error}</div>; // Display the error message
+	}
 
 	return (
 		<div className={styles.itemGrid}>
 			{items.map((item) => (
-				<ItemCard key={item.id} name={item.name} url={item.url} />
+				<ItemCard key={item.id} name={item.title.length > 25 ? item.title.substring(0, 22) + "..." : item.title} url={item.image} />
 			))}
 		</div>
 	);
