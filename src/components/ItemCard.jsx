084b9dc0bb48229fa84ba8ef7b8b227f2d95f5d9
adams <adamskiptalam0@gmail.com>
2025-03-28 00:24:43 +0300

    Style the item card for the shop

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,20 +1,20 @@
 import styles from "../styles/ItemCard.module.css"; // Assuming you have a CSS module for styling
 import { FaMinusSquare, FaPlusSquare, FaCartArrowDown } from "react-icons/fa";
 
-const ItemCard = () => {
+const ItemCard = ({ name, url }) => {
 	return (
 		<div className={styles.itemCard}>
 			<div className={styles.imageContainer}>
-                <img src="path/to/image.jpg" alt="Item" className={styles.itemImage} />
+                <img src={url} alt="Item" className={styles.itemImage} />
             </div>
 			<div className={styles.textContainer}>
-                <h3 className={styles.itemTitle}>Item Title</h3>
+                <h3 className={styles.itemTitle}>{name}</h3>
             </div>
 			<div className={styles.itemIcons}>
                 <FaMinusSquare className={styles.icon} />
                 <span className={styles.quantity}>1</span>
                 <FaPlusSquare className={styles.icon} />
-                <button className={styles.addCartBtn} type="button"><FaCartArrowDown className={styles.icon} /></button>
+                <FaCartArrowDown className={styles.icon} />
             </div>
 		</div>
 	);
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,15 @@
 import styles from '../styles/Shop.module.css';
 import Header from '../components/Header.jsx';
 import SearchBar from '../components/SearchBar.jsx';
+import ItemCard from '../components/ItemCard.jsx';
+import shoeImage from "../assets/shoe.jpg";
 
 export default function Shop() {    
     return (
         <div className={styles.shopContainer}>
             <Header />
             <SearchBar />
+            <ItemCard name ="adidas" url={shoeImage}/>
         </div>
     );
 }
